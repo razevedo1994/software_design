4c307a0088d8f5b3c6e5b3bb1f7e736f18683f9c
Rodrigo <razevedo.contato@gmail.com>
2026-04-22 08:39:39 -0300

    book catalogue

---
+++ ch01/book.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self, title: str, last: str, first: str):
+        self._title = title
+        self._last = last
+        self._first = first
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def last(self):
+        return self._last
+
+    @property
+    def first(self):
+        return self._first
+
+    def __str__(self):
+        return f"{{TITLE: '{self._title}', LAST: '{self._last}', FIRST: '{self.self._first}'}}"
