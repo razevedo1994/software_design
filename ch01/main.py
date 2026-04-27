bca1ebe628d82b66bc192c5af07dddbb16e5fcb3
Rodrigo <razevedo.contato@gmail.com>
2026-04-27 08:54:32 -0300

    Create main file to test the application

---
+++ ch01/main.py
@@ -0,0 +1,21 @@
+from book import Book
+from catalogue import Catalogue
+
+
+def fill(catalogue: Catalogue):
+    catalogue.add("Life of Pi", "Martel", "Yann")
+    catalogue.add("The Call of the Wilde", "London", "Jack")
+
+    catalogue.add("To Kill a Monckinbird", "Lee", "Harper")
+    catalogue.add("Little Woman", "Alcott", "Louisa")
+
+    catalogue.add("The Adventures of Sherlock Homles", "Doyle", "Arthur")
+    catalogue.add("And Then There Were None", "Christie", "Agatha")
+
+    catalogue.add("Carrie", "King", "Stephen")
+    catalogue.add("It: A Novel", "King", "Stephen")
+
+    catalogue.add("Frankenstein", "Shelley", "Mary")
+    catalogue.add("2001: A Space Odyssey", "Clarke", "Arthur")
+
+    catalogue.add("Ender's Game", "Card", "Orson")
