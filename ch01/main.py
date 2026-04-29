963d0653e25d24c682990843bea35431b6f5e16a
Rodrigo <razevedo.contato@gmail.com>
2026-04-29 07:40:30 -0300

    Add test scenario

--- ch01/main.py
+++ ch01/main.py
@@ -19,3 +19,37 @@ def fill(catalogue: Catalogue):
     catalogue.add("2001: A Space Odyssey", "Clarke", "Arthur")
 
     catalogue.add("Ender's Game", "Card", "Orson")
+
+def search(catalogue: Catalogue, target: Book):
+    print()
+    print("Find ", end="")
+    print(target)
+
+    matches = catalogue.find(target)
+
+    if len(matches) == 0:
+        print("No matches.")
+    else:
+        print("Matches:")
+
+        for book in matches:
+            print(" ", end="")
+            print(book)
+
+def test(catalogue: Catalogue):
+    target = Book("Life of Pi", "Martel", "Yann")
+    search(catalogue, target)
+
+    target = Book("", "King", "")
+    search(catalogue, target)
+
+    target = Book("1984", "Orwell", "George")
+    search(catalogue, target)
+
+    target = Book("", "", "")
+    search(catalogue, target)
+
+if __name__ == "__main__":
+    catalogue = Catalogue()
+    fill(catalogue)
+    test(catalogue)
